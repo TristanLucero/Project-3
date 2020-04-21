6a09b9ce31bfcf39748a760604e09c562d372dcd
Tristan Lucero <tristanandrewlucero@gmail.com>
2020-04-21 14:35:20 -0500

    Update dice.java

--- dice.java
+++ dice.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.*;
 
 public class Dice {
-    static int diceArray[] = new int[5]; //each dice in the diceArray holds the value of the symbols
+    static int diceArray[] = new int[6]; //each dice in the diceArray holds the value of the symbols
     /*0 = arrow
       1 = dynamite
       2 = bull's eye "1"
@@ -11,12 +11,14 @@ public class Dice {
       4 = beer
       5 = gatling
     */
-    static boolean diceRerolled[] = new boolean[5]; //gets updated everytime a dice is rolled, so dice can't be rolled again
+    static boolean diceRerolled[] = new boolean[6]; //gets updated everytime a dice is rolled, so dice can't be rolled again
     public int gatlingCount = 0; //how many times gatling has been rolled, default zero
     private int rerollsPlayer = 0; //rerolls player has done in total
-    private int previousDice[] = new int[5];
+    public static int dynamiteCount = 0;
 
-    public static int dynamiteCount = 0; //EDIT THIS AND ADD MORE TO IT
+    static private boolean saloonDice[] = new boolean[6];
+    static private boolean deadOrAliveDice[] = new boolean[6];
+    static private boolean expansionUsed = false;
 
 
     public void dice(){
@@ -32,8 +34,8 @@ public class Dice {
     }
 
     //only rolls 1 dice, doesn't need array just uses index.
-    public Dice rollOneDice(Dice dice, int diceIndex, Character character){
-        if (canBeRerolled(dice, diceIndex, character)){
+    public Dice rollOneDice(Dice dice, int diceIndex){
+        if (canBeRerolled(dice, diceIndex)){
             Random rand = new Random();
             diceArray[diceIndex] = rand.nextInt(6);
             diceRerolled[diceIndex] = true;
@@ -44,31 +46,31 @@ public class Dice {
     }
 
     //Rerolled dice, up to the full amount of dice. Array values set to true will be rerolled if they can be.
-    public Dice rerollDice(Dice dice, boolean diceToReroll[], Character character){
+    public Dice rerollDice(Dice dice, boolean diceToReroll[]){
         Random rand = new Random();
         boolean diceHaveBeenRerolled = false; //will be set to true if any dice is rerolled
 
-        if(diceToReroll[0] == true && canBeRerolled(dice, 0, character)){
+        if(diceToReroll[0] == true && canBeRerolled(dice, 0)){
             diceArray[0] = rand.nextInt(6);
             diceRerolled[0] = true;
             diceHaveBeenRerolled = true;
         }
-        if(diceToReroll[1] == true && canBeRerolled(dice, 1, character)){
+        if(diceToReroll[1] == true && canBeRerolled(dice, 1)){
             diceArray[1] = rand.nextInt(6);
             diceRerolled[1] = true;
             diceHaveBeenRerolled = true;
         }
-        if(diceToReroll[2] == true && canBeRerolled(dice, 2, character)){
+        if(diceToReroll[2] == true && canBeRerolled(dice, 2)){
             diceArray[2] = rand.nextInt(6);
             diceRerolled[2] = true;
             diceHaveBeenRerolled = true;
         }
-        if(diceToReroll[3] == true && canBeRerolled(dice, 3, character)){
+        if(diceToReroll[3] == true && canBeRerolled(dice, 3)){
             diceArray[3] = rand.nextInt(6);
             diceRerolled[3] = true;
             diceHaveBeenRerolled = true;
         }
-        if(diceToReroll[4] == true && canBeRerolled(dice, 4, character)){
+        if(diceToReroll[4] == true && canBeRerolled(dice, 4)){
             diceArray[4] = rand.nextInt(6);
             diceRerolled[4] = true;
             diceHaveBeenRerolled = true;
@@ -81,38 +83,65 @@ public class Dice {
     }
 
     //checks to see if dice can be rerolled or not at the index provided
-    public boolean canBeRerolled(Dice dice, int diceIndex, Character character){
+    public boolean canBeRerolled(Dice dice, int diceIndex){
         if(diceRerolled[diceIndex] == true) //if dice is rerolled, you can't reroll it again
             return false;
-        if(diceArray[diceIndex] == 1) { //if dice is dynamite, can't reroll unless they are character Black Jack
-            if(character.getAbilityNum() == 3)
-                return true;
-            else
-                return false;
-        }
+        //-----------------------------------------CHANGE FOR EXPANSION-----------------------
+        if(diceArray[diceIndex] == 1) //if dice is dynamite, can't reroll
+            return false;
         return true;
     }
 
     public String toString(int symbolIndex){ //stopped here lately
         String string = new String("");
-        if(symbolIndex == 0)
-            string = "Indian arrow";
-        else if(symbolIndex == 1)
-            string = "Dynamite";
-        else if(symbolIndex == 2)
-            string = "Bull's Eye \"1\"";
-        else if(symbolIndex == 3)
-            string = "Bull's Eye \"2\"";
-        else if(symbolIndex == 4)
-            string = "Beer";
-        else if(symbolIndex == 5)
-            string = "Gatling";
+        if(saloonDice[symbolIndex] == true){
+            if(symbolIndex == 0)
+                string = "Broken Arrow";
+            else if(symbolIndex == 1)
+                string = "Bullet";
+            else if(symbolIndex == 2)
+                string = "Double Bull's Eye \"1\"";
+            else if(symbolIndex == 3)
+                string = "Double Bull's Eye \"2\"";
+            else if(symbolIndex == 4)
+                string = "Double Beer";
+            else if(symbolIndex == 5)
+                string = "Double Gatling";
+        }
+        else if(deadOrAliveDice[symbolIndex] == true){
+            if(symbolIndex == 0)
+                string = "Indian Arrow";
+            else if(symbolIndex == 1)
+                string = "Dynamite";
+            else if(symbolIndex == 2)
+                string = "Whiskey Bottle";
+            else if(symbolIndex == 3)
+                string = "Fight a Dual";
+            else if(symbolIndex == 4)
+                string = "Fight a Dual";
+            else if(symbolIndex == 5)
+                string = "Gatling";
+        }
+        else{
+            if(symbolIndex == 0)
+                string = "Indian Arrow";
+            else if(symbolIndex == 1)
+                string = "Dynamite";
+            else if(symbolIndex == 2)
+                string = "Bull's Eye \"1\"";
+            else if(symbolIndex == 3)
+                string = "Bull's Eye \"2\"";
+            else if(symbolIndex == 4)
+                string = "Beer";
+            else if(symbolIndex == 5)
+                string = "Gatling";
+        }
         return string;
     }
 
-    //checks if gatling is ready which is normally 3 gatling rolls, or 2 with Willy The Kid
-    public boolean gatlingReady(Character character){
-        if(gatlingCount >= 3 || gatlingCount >= 2 && character.getAbilityNum() == 16) {
+    //if gatling is >= 3 returns true
+    public boolean gatlingReady(){
+        if(gatlingCount >= 3) {
             gatlingCount = 0;
             return true;
         }
@@ -120,8 +149,9 @@ public class Dice {
             return false;
     }
 
-    public boolean dynamiteReady(Character character){
-        if(dynamiteCount >= 3 /*|| gatlingCount >= 2 && character.getAbilityNum() == 16*/) {
+    //if dynamite is >= 3 returns true
+    public boolean dynamiteReady(){
+        if(dynamiteCount >= 3) {
             dynamiteCount = 0;
             return true;
         }
@@ -129,14 +159,53 @@ public class Dice {
             return false;
     }
 
+    //calculate gatling and dynamite counts for the current dice in hand
+    //-------------------------------------NEED TO CHANGE FOR EXPANSION----------------------------------------------
     public void calcDiceCounts(){
         gatlingCount = 0;
         dynamiteCount = 0;
         for(int i = 0; i < diceArray.length; i++){
-            if(diceArray[i] == 5)
-                gatlingCount++;
-            if(diceArray[i] == 1)
-                dynamiteCount++;
+            if(saloonDice[i] == true){
+                if(diceArray[i] == 5)
+                    gatlingCount++;
+            }
+            if(deadOrAliveDice[i] == true){
+                if(diceArray[i] == 5)
+                    gatlingCount++;
+                if(diceArray[i] == 1)
+                    dynamiteCount++;
+            }
+            else{
+                if (diceArray[i] == 5)
+                    gatlingCount++;
+                if (diceArray[i] == 1)
+                    dynamiteCount++;
+            }
+        }
+    }
+
+    //Call the function to use expansion in dice class,
+    // pass the number of dice you want to use from saloon expansion, either 1 or 2 or them.
+    //2 deadOrAliveDice / black dice used automatically
+    public static void useExpansion(int saloonDiceToUse){
+        expansionUsed = true;
+        deadOrAliveDice[4] = true;
+        deadOrAliveDice[5] = true;
+        if(saloonDiceToUse == 1)
+            saloonDice[3] = true;
+        else if(saloonDiceToUse == 2){
+            saloonDice[2] = true;
+            saloonDice[3] = true;
+        }
+    }
+
+    //sets expansion to not be used, sets all dice to default dice
+    public static void dontUseExpansion(){
+        expansionUsed = false;
+        for (int i = 0; i < diceArray.length; i++) {
+            deadOrAliveDice[i] = false;
+            saloonDice[i] = false;
+            System.out.println(i);
         }
     }
 
